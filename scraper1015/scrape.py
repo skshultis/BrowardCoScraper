608c45f28b3d36fcd793f6f0bd473e3b77e45849
Sarah K Shultis <skshultis@Sks-MacBook-Air.fios-router.home>
2016-10-20 17:09:12 -0400

    cleaning it up a little

--- scraper1015/scrape.py
+++ scraper1015/scrape.py
@@ -1,3 +1,4 @@
+import csv
 import requests
 from BeautifulSoup import BeautifulSoup
 
@@ -6,4 +7,15 @@ response = requests.get(url)
 html = response.content
 
 soup = BeautifulSoup(html)
-print soup.prettify()
+table = soup.find('table', attrs={'class': 'datagrid'})
+
+list_of_rows = []
+for row in table.findAll('tr'):
+    list_of_cells = []
+    for cell in row.findAll('td'):
+        text = cell.text.replace('&nbsp;', '')
+        text = cell.text.replace('M&nsbp', '')
+        list_of_cells.append(text)
+    list_of_rows.append(list_of_cells)
+
+    print list_of_rows
